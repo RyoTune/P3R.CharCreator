c854e68af658e9f58025a370b1968339539d1328
MirrorTuneZM <zutomayofan50@gmail.com>
2024-03-25 08:06:10 -0500

    Setup logger.

--- P3R.CharCreator.Reloaded/Mod.cs
+++ P3R.CharCreator.Reloaded/Mod.cs
@@ -2,43 +2,48 @@
 using P3R.CharCreator.Reloaded.Template;
 using Reloaded.Hooks.ReloadedII.Interfaces;
 using Reloaded.Mod.Interfaces;
+using System.Drawing;
 
-namespace P3R.CharCreator.Reloaded
+namespace P3R.CharCreator.Reloaded;
+
+public class Mod : ModBase
 {
-    public class Mod : ModBase
+    public const string NAME = "P3R.CharCreator";
+
+    private readonly IModLoader modLoader;
+    private readonly IReloadedHooks? hooks;
+    private readonly ILogger log;
+    private readonly IMod owner;
+
+    private Config config;
+    private readonly IModConfig modConfig;
+
+    public Mod(ModContext context)
     {
-        private readonly IModLoader modLoader;
-        private readonly IReloadedHooks? hooks;
-        private readonly ILogger log;
-        private readonly IMod owner;
+        this.modLoader = context.ModLoader;
+        this.hooks = context.Hooks;
+        this.log = context.Logger;
+        this.owner = context.Owner;
+        this.config = context.Configuration;
+        this.modConfig = context.ModConfig;
 
-        private Config config;
-        private readonly IModConfig modConfig;
-
-        public Mod(ModContext context)
-        {
-            this.modLoader = context.ModLoader;
-            this.hooks = context.Hooks;
-            this.log = context.Logger;
-            this.owner = context.Owner;
-            this.config = context.Configuration;
-            this.modConfig = context.ModConfig;
-        }
-
-        #region Standard Overrides
-        public override void ConfigurationUpdated(Config configuration)
-        {
-            // Apply settings from configuration.
-            // ... your code here.
-            config = configuration;
-            log.WriteLine($"[{modConfig.ModId}] Config Updated: Applying");
-        }
-        #endregion
-
-        #region For Exports, Serialization etc.
-#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
-        public Mod() { }
-#pragma warning restore CS8618
-        #endregion
+        Log.Initialize(NAME, this.log, Color.White);
+        Log.LogLevel = this.config.LogLevel;
     }
+
+    #region Standard Overrides
+    public override void ConfigurationUpdated(Config configuration)
+    {
+        // Apply settings from configuration.
+        // ... your code here.
+        config = configuration;
+        log.WriteLine($"[{modConfig.ModId}] Config Updated: Applying");
+    }
+    #endregion
+
+    #region For Exports, Serialization etc.
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+    public Mod() { }
+#pragma warning restore CS8618
+    #endregion
 }
