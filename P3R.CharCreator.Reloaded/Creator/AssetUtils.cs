2b88c646240ffd287e79f73915b10ebcc1e5a381
MirrorTuneZM <zutomayofan50@gmail.com>
2024-04-01 22:23:43 -0500

    Implement basic functionality.

---
+++ P3R.CharCreator.Reloaded/Creator/AssetUtils.cs
@@ -0,0 +1,61 @@
+﻿using P3R.CharCreator.Reloaded.Creator.Types;
+
+namespace P3R.CharCreator.Reloaded.Creator;
+
+internal static class AssetUtils
+{
+    /// <summary>
+    /// Gets the expected asset file for the given character's costume ID and asset type.
+    /// </summary>
+    /// <param name="character">Character.</param>
+    /// <param name="costumeId">Costume ID.</param>
+    /// <param name="type">Asset type.</param>
+    /// <returns></returns>
+    public static string? GetAssetFile(Character character, int costumeId, CostumeAssetType type)
+    {
+        string? assetFile = type switch
+        {
+            CostumeAssetType.Base_Mesh => $"/Game/Xrd777/Characters/Player/PC{GetCharIdString(character)}/Models/SK_PC{GetCharIdString(character)}_BaseSkeleton.uasset",
+            CostumeAssetType.Costume_Mesh => $"/Game/Xrd777/Characters/Player/PC{GetCharIdString(character)}/Models/SK_PC{GetCharIdString(character)}_C{costumeId:000}.uasset",
+            CostumeAssetType.Hair_Mesh => $"/Game/Xrd777/Characters/Player/PC{GetCharIdString(character)}/Models/SK_PC{GetCharIdString(character)}_H{costumeId:000}.uasset",
+            CostumeAssetType.Face_Mesh => $"/Game/Xrd777/Characters/Player/PC{GetCharIdString(character)}/Models/SK_PC{GetCharIdString(character)}_F{costumeId:000}.uasset",
+
+            CostumeAssetType.Base_Anim => $"/Game/Xrd777/Characters/Player/PC{GetCharIdString(character)}/ABP_PC{GetCharIdString(character)}.uasset",
+            CostumeAssetType.Costume_Anim => "/CharacterBase/Human/Blueprints/Animation/ABP_CH_CostumeBase.uasset",
+            CostumeAssetType.Hair_Anim => "/CharacterBase/Human/Blueprints/Animation/ABP_CH_HairBase.uasset",
+            CostumeAssetType.Face_Anim => null,
+            _ => throw new Exception(),
+        };
+
+        return assetFile;
+    }
+
+    public static string? GetAssetFile(Character character, Outfit outfit, CostumeAssetType type)
+        => GetAssetFile(character, (int)outfit, type);
+
+    /// <summary>
+    /// Gets the expected asset path from asset file path.
+    /// Simply removes the .uasset extension and/or adds the game content path.
+    /// </summary>
+    /// <param name="assetFile">Asset .uasset file path.</param>
+    /// <returns>Asset path.</returns>
+    public static string GetAssetPath(string assetFile)
+    {
+        var adjustedPath = assetFile.Replace('\\', '/').Replace(".uasset", string.Empty);
+        if (!adjustedPath.StartsWith("/Game/"))
+        {
+            adjustedPath = $"/Game/{adjustedPath}";
+        }
+
+        return adjustedPath;
+    }
+
+    public static string? GetAssetPath(Character character, int costumeId, CostumeAssetType type)
+    {
+        var assetFile = GetAssetFile(character, costumeId, type);
+        return assetFile != null ? GetAssetPath(assetFile) : null;
+    }
+
+    public static string GetCharIdString(Character character)
+        => ((int)character).ToString("0000");
+}
---
+++ P3R.CharCreator.Reloaded/Creator/CreatorService.cs
@@ -0,0 +1,87 @@
+﻿using P3R.CharCreator.Reloaded.Configuration;
+using P3R.CharCreator.Reloaded.Creator.Types;
+using Unreal.ObjectsEmitter.Interfaces;
+
+namespace P3R.CharCreator.Reloaded.Creator;
+
+internal class CreatorService
+{
+    private readonly IUnreal unreal;
+
+    public CreatorService(IUnreal unreal)
+    {
+        this.unreal = unreal;
+    }
+
+    public void Apply(Config config)
+    {
+        // Base.
+        this.RedirectToCharAsset("/Game/Xrd777/Characters/Player/PC0001/Models/SK_PC0001_BaseSkelton.uasset", config.BaseSkeleton);
+        this.RedirectToCharAsset("/Game/Xrd777/Characters/Player/PC0001/Models/SK_PC0001_F000.uasset", config.FaceMesh);
+        this.RedirectToCharAsset("/Game/Xrd777/Characters/Player/PC0001/Models/SK_PC0001_H000.uasset", config.HairMesh);
+
+        // Animations.
+        this.RedirectToCharAsset("/Game/Xrd777/Characters/Data/DataAsset/Player/PC0001/DA_PC0001_CommonAnim.uasset", config.CommonAnim);
+        this.RedirectToCharAsset("/Game/Xrd777/Characters/Data/DataAsset/Player/PC0001/DA_PC0001_CombineAnim.uasset", config.CombineAnim);
+        this.RedirectToCharAsset("/Game/Xrd777/Characters/Data/DataAsset/Player/PC0001/DA_PC0001_EventAnim.uasset", config.EventAnim);
+        this.RedirectToCharAsset("/Game/Xrd777/Characters/Data/DataAsset/Player/PC0001/DA_PC0001_FaceAnim.uasset", config.FaceAnim);
+
+        // Outfits.
+        this.RedirectPlayerOutfit(Outfit.Summer_Uniform, config.SummerUniform);
+        this.RedirectPlayerOutfit(Outfit.Winter_Uniform, config.WinterUniform);
+        this.RedirectPlayerOutfit(Outfit.Summer_Casual, config.SummerCasual);
+        this.RedirectPlayerOutfit(Outfit.Winter_Casual, config.WinterCasual);
+    }
+
+    private void RedirectToCharAsset(string assetFile, Character redirectChar)
+    {
+        // No need to redirect player assets back to player.
+        if (redirectChar == Character.Player)
+        {
+            return;
+        }
+
+        var fnames = new AssetFNames(assetFile);
+
+        if (redirectChar >= Character.Player && redirectChar <= Character.Shinjiro)
+        {
+            this.unreal.AssignFName(Mod.NAME, fnames.AssetName, fnames.AssetName.Replace("0001", $"{(int)redirectChar:0000}"));
+            this.unreal.AssignFName(Mod.NAME, fnames.AssetPath, fnames.AssetPath.Replace("0001", $"{(int)redirectChar:0000}"));
+        }
+        else if (redirectChar == Character.FEMC)
+        {
+            this.unreal.AssignFName(Mod.NAME, fnames.AssetName, fnames.AssetName.Replace("0001", "0002").Replace("F000", "F999").Replace("H000", "H999"));
+            this.unreal.AssignFName(Mod.NAME, fnames.AssetPath, fnames.AssetPath.Replace("0001", "0002").Replace("F000", "F999").Replace("H000", "H999"));
+        }
+        else
+        {
+            this.unreal.AssignFName(Mod.NAME, fnames.AssetName, fnames.AssetName.Replace("PC0001", $"SC{(int)redirectChar:0000}"));
+            this.unreal.AssignFName(Mod.NAME, fnames.AssetPath, fnames.AssetPath.Replace("PC0001", $"SC{(int)redirectChar:0000}").Replace("Player", "Sub", StringComparison.OrdinalIgnoreCase));
+        }
+    }
+
+    private void RedirectPlayerOutfit(Outfit outfit, Character redirectChar)
+    {
+        var assetFile = AssetUtils.GetAssetFile(Character.Player, outfit, CostumeAssetType.Costume_Mesh)!;
+
+        var fnames = new AssetFNames(assetFile);
+
+        if (redirectChar >= Character.Player && redirectChar <= Character.Shinjiro)
+        {
+            this.unreal.AssignFName(Mod.NAME, fnames.AssetName, fnames.AssetName.Replace("0001", $"{(int)redirectChar:0000}"));
+            this.unreal.AssignFName(Mod.NAME, fnames.AssetPath, fnames.AssetPath.Replace("0001", $"{(int)redirectChar:0000}"));
+        }
+        else
+        {
+            this.unreal.AssignFName(Mod.NAME, fnames.AssetName, fnames.AssetName.Replace("PC0001", $"SC{(int)redirectChar:0000}"));
+            this.unreal.AssignFName(Mod.NAME, fnames.AssetPath, fnames.AssetPath.Replace("PC0001", $"SC{(int)redirectChar:0000}").Replace("Player", "Sub", StringComparison.OrdinalIgnoreCase));
+        }
+    }
+
+    private record AssetFNames(string AssetFile)
+    {
+        public string AssetName { get; } = Path.GetFileNameWithoutExtension(AssetFile);
+
+        public string AssetPath { get; } = AssetUtils.GetAssetPath(AssetFile);
+    };
+}
