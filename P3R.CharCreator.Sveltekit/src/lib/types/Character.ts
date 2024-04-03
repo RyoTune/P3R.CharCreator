975fd8d8ad0ab86f5ad1a072affc91cecf57e3da
MirrorTuneZM <zutomayofan50@gmail.com>
2024-04-02 19:30:34 -0500

    Add JS character data types.

---
+++ P3R.CharCreator.Sveltekit/src/lib/types/Character.ts
@@ -0,0 +1,44 @@
+export enum Character {
+  Default = 1,
+  Yukari,
+  Stupei,
+  Akihiko,
+  Mitsuru,
+  Fuuka,
+  Aigis,
+  //Ken,
+  //Koromaru,
+  Shinjiro = 10,
+
+  // Side-characters.
+  Kenji = 101,
+  Hidetoshi = 102,
+  //Bunkichi,
+  //Mitsuko,
+  Kazushi = 105,
+  Yuko = 106,
+  Keisuke = 108,
+  Chihiro = 109,
+  //Maiko,
+  //Pharos = 110,
+  Andre_Laurent_Jean_Geraux = 112,
+  Tanaka = 113,
+  //Mutatsu,
+  Mamoru = 115,
+  //Akinari,
+
+  // Should probably be disabled since meshes will bug out
+  // but might be funny.
+  Igor = 201,
+
+  Elizabeth,
+
+  Takaya = 211,
+  Jin,
+  Chidori,
+
+  Ryoji = 221,
+  Ikutsuki,
+  Natsuki,
+  Takeharu
+}
---
+++ P3R.CharCreator.Sveltekit/src/lib/types/Protag.ts
@@ -0,0 +1,4 @@
+export enum Protag {
+	MC,
+	FEMC
+}
