c4ae4ba667c9f215b6d34abc9e8afc082128e238
MirrorTuneZM <zutomayofan50@gmail.com>
2024-04-03 16:49:55 -0500

    Implement outfits assets tab.

---
+++ P3R.CharCreator.Sveltekit/src/routes/OutfitsForm.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+  import { Character } from '$lib/types/Character';
+  import { charConfig, costumes, gameCostumes } from './ConfigStore';
+  $: outfitEntries = gameCostumes.filter(
+    (x) => x.character == $charConfig.base.character && x.outfit != undefined
+  );
+</script>
+
+<h1 class="title">Outfits</h1>
+<h2 class="subtitle">
+  Pick and choose what outfits you want to use <strong>in-place</strong> of existing outfits.
+</h2>
+
+<div class="box">
+  {#each outfitEntries as entry}
+    <div class="field">
+      <label for={`outfit-${entry.outfit}`} class="label title is-4">{entry.name}</label>
+      <div class="control">
+        <div class="select">
+          <select id={`outfit-${entry.outfit}`} bind:value={$charConfig.outfits[entry.outfit ?? 0]}>
+            <option value={null}>Default</option>
+            {#each $costumes as costume}
+              <option value={costume.assetPath}
+                >{`${Character[costume.character]}'s ${costume.name}`}</option
+              >
+            {/each}
+          </select>
+        </div>
+      </div>
+    </div>
+  {/each}
+</div>
