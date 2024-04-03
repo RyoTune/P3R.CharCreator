0b3ab75769b2775fd3224ca4a4b99b5335ae4309
MirrorTuneZM <zutomayofan50@gmail.com>
2024-04-02 20:22:21 -0500

    Adjust asset path if character is non-player character.

--- P3R.CharCreator.Sveltekit/src/lib/utils/AssetUtils.ts
+++ P3R.CharCreator.Sveltekit/src/lib/utils/AssetUtils.ts
@@ -1,19 +1,59 @@
 import { AssetType } from '$lib/types/AssetType';
-import type { Character } from '$lib/types/Character';
+import { Character } from '$lib/types/Character';
 
-export function GetAssetFile(char: Character, costumeId: number, assetType: AssetType): string {
+export function GetAssetFile(char: Character, assetType: AssetType, costumeId: number = 0): string {
   switch (assetType) {
-    case AssetType.Base_Mesh:
-      return `/Game/Xrd777/Characters/Player/PC${FormatCharId(char)}/Models/SK_PC${FormatCharId(char)}_BaseSkeleton.uasset`;
-    case AssetType.Costume_Mesh:
-      return `/Game/Xrd777/Characters/Player/PC${FormatCharId(char)}/Models/SK_PC${FormatCharId(char)}_C${FormatCostumeId(costumeId)}.uasset`;
-    case AssetType.Face_Mesh:
-      return `/Game/Xrd777/Characters/Player/PC${FormatCharId(char)}/Models/SK_PC${FormatCharId(char)}_F${FormatCostumeId(costumeId)}.uasset`;
-    case AssetType.Hair_Mesh:
-      return `/Game/Xrd777/Characters/Player/PC${FormatCharId(char)}/Models/SK_PC${FormatCharId(char)}_H${FormatCostumeId(costumeId)}.uasset`;
+    case AssetType.BaseMesh:
+      return FormatAssetPath(
+        `/Game/Xrd777/Characters/Player/PC${FormatCharId(char)}/Models/SK_PC${FormatCharId(char)}_BaseSkeleton.uasset`,
+        char
+      );
+    case AssetType.CostumeMesh:
+      return FormatAssetPath(
+        `/Game/Xrd777/Characters/Player/PC${FormatCharId(char)}/Models/SK_PC${FormatCharId(char)}_C${FormatCostumeId(costumeId)}.uasset`,
+        char
+      );
+    case AssetType.FaceMesh:
+      return FormatAssetPath(
+        `/Game/Xrd777/Characters/Player/PC${FormatCharId(char)}/Models/SK_PC${FormatCharId(char)}_F${FormatCostumeId(costumeId)}.uasset`,
+        char
+      );
+    case AssetType.HairMesh:
+      return FormatAssetPath(
+        `/Game/Xrd777/Characters/Player/PC${FormatCharId(char)}/Models/SK_PC${FormatCharId(char)}_H${FormatCostumeId(costumeId)}.uasset`,
+        char
+      );
+    case AssetType.CommonAnim:
+      return FormatAssetPath(
+        `/Game/Xrd777/Characters/Data/DataAsset/Player/PC${FormatCharId(char)}/DA_PC${FormatCharId(char)}_CommonAnim.uasset`,
+        char
+      );
+    case AssetType.CombineAnim:
+      return FormatAssetPath(
+        `/Game/Xrd777/Characters/Data/DataAsset/Player/PC${FormatCharId(char)}/DA_PC${FormatCharId(char)}_CombineAnim.uasset`,
+        char
+      );
+    case AssetType.EventAnim:
+      return FormatAssetPath(
+        `/Game/Xrd777/Characters/Data/DataAsset/Player/PC${FormatCharId(char)}/DA_PC${FormatCharId(char)}_EventAnim.uasset`,
+        char
+      );
+    case AssetType.FaceAnim:
+      return FormatAssetPath(
+        `/Game/Xrd777/Characters/Data/DataAsset/Player/PC${FormatCharId(char)}/DA_PC${FormatCharId(char)}_FaceAnim.uasset`,
+        char
+      );
     default:
       return '';
   }
+}
+
+function FormatAssetPath(path: string, char: Character) {
+  if (char >= Character.Player && char <= Character.Shinjiro) {
+    return path;
+  }
+
+  return path.replaceAll('PC', 'SC').replaceAll('Player', 'Sub');
 }
 
 export function FormatCharId(char: Character) {
