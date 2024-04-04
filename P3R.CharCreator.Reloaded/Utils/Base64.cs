1a2284270a22276ee70fd9b96b39c64d57ccae64
MirrorTuneZM <zutomayofan50@gmail.com>
2024-04-04 04:26:16 -0500

    Add utility functions.

---
+++ P3R.CharCreator.Reloaded/Utils/Base64.cs
@@ -0,0 +1,16 @@
+﻿namespace P3R.CharCreator.Reloaded.Utils;
+
+internal static class Base64
+{
+    public static string Encode(string input)
+    {
+        var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(input);
+        return Convert.ToBase64String(plainTextBytes);
+    }
+
+    public static string Decode(string input)
+    {
+        var inputBytes = Convert.FromBase64String(input);
+        return System.Text.Encoding.UTF8.GetString(inputBytes);
+    }
+}
---
+++ P3R.CharCreator.Reloaded/Utils/Common.cs
@@ -0,0 +1,12 @@
+﻿namespace P3R.CharCreator.Reloaded.Utils;
+
+internal static class Common
+{
+    public static void DoIfString(string? str, Action<string> action)
+    {
+        if (string.IsNullOrEmpty(str) == false)
+        {
+            action(str);
+        }
+    }
+}
