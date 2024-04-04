864442f3d4da0c956a6aed4da0363d7ae2d38eb4
MirrorTuneZM <zutomayofan50@gmail.com>
2024-04-04 04:26:00 -0500

    Update types.

---
+++ P3R.CharCreator.Reloaded/Creator/Types/AssetType.cs
@@ -0,0 +1,14 @@
+﻿namespace P3R.CharCreator.Reloaded.Creator;
+
+public enum AssetType
+{
+    BaseMesh,
+    CostumeMesh,
+    HairMesh,
+    FaceMesh,
+
+    CommonAnim,
+    CombineAnim,
+    EventAnim,
+    FaceAnim,
+}
