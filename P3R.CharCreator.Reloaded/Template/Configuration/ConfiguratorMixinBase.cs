ca568c689f238f3b9d5ff588f201536d4be1c1e9
MirrorTuneZM <zutomayofan50@gmail.com>
2024-04-04 01:22:05 -0500

    Add support for importing character assets from Costume Framework mods, FEMC project, and asset packs.

--- P3R.CharCreator.Reloaded/Template/Configuration/ConfiguratorMixinBase.cs
+++ P3R.CharCreator.Reloaded/Template/Configuration/ConfiguratorMixinBase.cs
@@ -1,55 +1,240 @@
 ﻿using P3R.CharCreator.Reloaded.Configuration;
+using P3R.CharCreator.Reloaded.Creator;
+using P3R.CharCreator.Reloaded.Creator.Types;
 using Reloaded.Mod.Interfaces;
+using System.Text.Json;
 
-namespace P3R.CharCreator.Reloaded.Template.Configuration
+namespace P3R.CharCreator.Reloaded.Template.Configuration;
+
+/// <summary>
+/// Creates the various different configurations used by the mod.
+/// These configurations are available in the dropdown in Reloaded launcher. 
+/// </summary>
+public class ConfiguratorMixinBase
 {
     /// <summary>
-    /// Creates the various different configurations used by the mod.
-    /// These configurations are available in the dropdown in Reloaded launcher. 
+    /// Defines the configuration items to create.
     /// </summary>
-    public class ConfiguratorMixinBase
+    /// <param name="configFolder">Folder storing the configuration.</param>
+    public virtual IUpdatableConfigurable[] MakeConfigurations(string configFolder)
     {
-        /// <summary>
-        /// Defines the configuration items to create.
-        /// </summary>
-        /// <param name="configFolder">Folder storing the configuration.</param>
-        public virtual IUpdatableConfigurable[] MakeConfigurations(string configFolder)
+        var config = Configurable<Config>.FromFile(Path.Combine(configFolder, "Config.json"), "Default Config");
+
+        try
         {
-            // You can add any Configurable here.
-            return new IUpdatableConfigurable[]
+            config.ImportMods = GetImportModsData(configFolder);
+        }
+        catch (Exception)
+        {
+        }
+
+        // You can add any Configurable here.
+        return new IUpdatableConfigurable[]
+        {
+            //Configurable<Config>.FromFile(Path.Combine(configFolder, "Config.json"), "Default Config")
+            config,
+        };
+    }
+
+    /// <summary>
+    /// Allows for custom launcher/configurator implementation.
+    /// If you have your own configuration program/code, run that code here and return true, else return false.
+    /// </summary>
+    public virtual bool TryRunCustomConfiguration(Configurator configurator)
+    {
+        return false;
+    }
+
+    private static string GetImportModsData(string configFolder)
+    {
+        var appConfigFile = Path.GetFullPath($"{configFolder}/../../../apps/p3r.exe/appconfig.json");
+        var appConfig = ReloadedParser.ParseAppConfig(appConfigFile);
+
+        var charAssets = new List<CharacterAssets>();
+        var modsDir = Path.GetFullPath($"{configFolder}/../../../mods");
+        foreach (var modDir in Directory.EnumerateDirectories(modsDir))
+        {
+            var modConfig = ReloadedParser.ParseModConfig(Path.Join(modDir, "ModConfig.json"));
+            if (!appConfig.EnabledMods.Contains(modConfig.ModId))
             {
-                Configurable<Config>.FromFile(Path.Combine(configFolder, "Config.json"), "Default Config")
-            };
+                continue;
+            }
+
+            try
+            {
+                AssetsFromCostumeFramework(charAssets, modDir);
+                AssetsFromPack(charAssets, modDir);
+
+                if (modConfig.ModId == "p3rpc.femc")
+                {
+                    AssetsFromFemc(charAssets);
+                }
+            }
+            catch (Exception)
+            {
+            }
         }
 
-        /// <summary>
-        /// Allows for custom launcher/configurator implementation.
-        /// If you have your own configuration program/code, run that code here and return true, else return false.
-        /// </summary>
-        public virtual bool TryRunCustomConfiguration(Configurator configurator)
+        return EncodeBase64(JsonSerializer.Serialize(charAssets, new JsonSerializerOptions() { WriteIndented = true, PropertyNamingPolicy = JsonNamingPolicy.CamelCase }));
+    }
+
+    private static void AssetsFromPack(List<CharacterAssets> charAssets, string modDir)
+    {
+        var assetsDir = Path.Join(modDir, "UnrealEssentials", "P3R", "Content", "char-assets");
+        if (!Directory.Exists(assetsDir))
         {
-            return false;
+            return;
         }
 
-        #region Config Migration (Must implement if coming from old mod template with config in mod folder)
-        /// <summary>
-        /// Migrates from the old config location (usually mod folder) to the newer config location (separate folder).
-        /// </summary>
-        /// <param name="oldDirectory">Old directory containing the mod configs.</param>
-        /// <param name="newDirectory">New directory pointing to user config folder.</param>
-        public virtual void Migrate(string oldDirectory, string newDirectory)
+        var faceAssetsDir = Path.Join(assetsDir, "face");
+        if (Directory.Exists(faceAssetsDir))
         {
-            // Uncomment to move files from older to newer config directory.
-            // TryMoveFile("Config.json");
+            AssetsFromFolder(charAssets, faceAssetsDir, AssetType.FaceMesh);
+        }
+
+        var hairAssetsDir = Path.Join(assetsDir, "hair");
+        if (Directory.Exists(hairAssetsDir))
+        {
+            AssetsFromFolder(charAssets, hairAssetsDir, AssetType.HairMesh);
+        }
+
+        var outfitsAssetsDir = Path.Join(assetsDir, "outfits");
+        if (Directory.Exists(outfitsAssetsDir))
+        {
+            AssetsFromFolder(charAssets, outfitsAssetsDir, AssetType.CostumeMesh);
+        }
+    }
+
+    private static void AssetsFromFolder(List<CharacterAssets> charAssets, string assetsDir, AssetType assetType)
+    {
+        foreach (var file in Directory.EnumerateFiles(assetsDir, "*.uasset", SearchOption.AllDirectories))
+        {
+            charAssets.Add(new(Path.GetFileNameWithoutExtension(file), Character.NONE, AssetUtils.GetAssetPath(file), assetType));
+        }
+    }
+
+    private static void AssetsFromCostumeFramework(List<CharacterAssets> charAssets, string modDir)
+    {
+        var costumesDir = Path.Join(modDir, "UnrealEssentials", "P3R", "Content", "Costumes");
+        if (!Directory.Exists(costumesDir))
+        {
+            return;
+        }
+
+        foreach (var file in Directory.EnumerateFiles(costumesDir, "*.uasset", SearchOption.AllDirectories))
+        {
+            var costumeName = Path.GetFileName(Path.GetDirectoryName(file))!;
+            var character = Enum.Parse<Character>(Path.GetFileName(Path.GetDirectoryName(Path.GetDirectoryName(file)))!);
+            var fileName = Path.GetFileName(file).ToLower();
+            var assetFile = AssetUtils.GetAssetPath(file);
+
+            switch (fileName)
+            {
+                case "costume-mesh.uasset":
+                    charAssets.Add(new(costumeName, character, assetFile, AssetType.CostumeMesh));
+                    break;
+                case "hair-mesh.uasset":
+                    charAssets.Add(new(costumeName, character, assetFile, AssetType.HairMesh));
+                    break;
+                case "face-mesh.uasset":
+                    charAssets.Add(new(costumeName, character, assetFile, AssetType.FaceMesh));
+                    break;
+                default: break;
+            }
+        }
+    }
+
+    private static void AssetsFromFemc(List<CharacterAssets> charAssets)
+    {
+        charAssets.Add(new("FEMC Uniform & Armband", Character.Player, GetFemcCostume(999), AssetType.CostumeMesh));
+        charAssets.Add(new("FEMC SEES Uniform", Character.Player, GetFemcCostume(998), AssetType.CostumeMesh));
+        charAssets.Add(new("FEMC Summer Uniform", Character.Player, GetFemcCostume(991), AssetType.CostumeMesh));
+        charAssets.Add(new("FEMC Winter Uniform", Character.Player, GetFemcCostume(992), AssetType.CostumeMesh));
+        charAssets.Add(new("FEMC Summer Garb", Character.Player, GetFemcCostume(993), AssetType.CostumeMesh));
+        charAssets.Add(new("FEMC Winter Garb", Character.Player, GetFemcCostume(994), AssetType.CostumeMesh));
+    }
+
+    private static string EncodeBase64(string input)
+    {
+        var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(input);
+        return Convert.ToBase64String(plainTextBytes);
+    }
+
+    private static string GetFemcCostume(int costumeId)
+        => AssetUtils.GetAssetFile(Character.Yukari, 0, CostumeAssetType.Costume_Mesh)!.Replace("_C000", $"_C{costumeId:000}");
+
+    #region Config Migration (Must implement if coming from old mod template with config in mod folder)
+    /// <summary>
+    /// Migrates from the old config location (usually mod folder) to the newer config location (separate folder).
+    /// </summary>
+    /// <param name="oldDirectory">Old directory containing the mod configs.</param>
+    /// <param name="newDirectory">New directory pointing to user config folder.</param>
+    public virtual void Migrate(string oldDirectory, string newDirectory)
+    {
+        // Uncomment to move files from older to newer config directory.
+        // TryMoveFile("Config.json");
 
 #pragma warning disable CS8321
-            void TryMoveFile(string fileName)
-            {
-                try { File.Move(Path.Combine(oldDirectory, fileName), Path.Combine(newDirectory, fileName)); }
-                catch (Exception) { /* Ignored */ }
-            }
-#pragma warning restore CS8321
+        void TryMoveFile(string fileName)
+        {
+            try { File.Move(Path.Combine(oldDirectory, fileName), Path.Combine(newDirectory, fileName)); }
+            catch (Exception) { /* Ignored */ }
         }
-        #endregion
+#pragma warning restore CS8321
     }
+    #endregion
+}
+
+internal static class ReloadedParser
+{
+    public static ReloadedModConfig ParseModConfig(string file)
+    {
+        try
+        {
+            return JsonSerializer.Deserialize<ReloadedModConfig>(File.ReadAllText(file)) ?? throw new Exception();
+        }
+        catch (Exception)
+        {
+            return new();
+        }
+    }
+
+    public static ReloadedAppConfig ParseAppConfig(string file)
+    {
+        try
+        {
+            return JsonSerializer.Deserialize<ReloadedAppConfig>(File.ReadAllText(file)) ?? throw new Exception();
+        }
+        catch (Exception)
+        {
+            return new();
+        }
+    }
+}
+
+internal class ReloadedModConfig
+{
+    public string ModId { get; set; } = string.Empty;
+
+    public string ModName { get; set; } = string.Empty;
+}
+
+internal class ReloadedAppConfig
+{
+    public string[] EnabledMods { get; set; } = Array.Empty<string>();
+}
+
+internal record CharacterAssets(string Name, Character Character, string AssetPath, AssetType AssetType);
+
+internal enum AssetType
+{
+    BaseMesh,
+    CostumeMesh,
+    HairMesh,
+    FaceMesh,
+
+    CommonAnim,
+    CombineAnim,
+    EventAnim,
+    FaceAnim
 }
